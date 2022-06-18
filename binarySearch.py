b213ab82a22b1c852e37e222799aa00b62fdbd1e
Pavan Bodaballa <impkb19@gmail.com>
2022-06-18 10:35:50 -0400

    Added additional tests

--- binarySearch.py
+++ binarySearch.py
@@ -1,14 +1,26 @@
 class BinarySearch:
+    
+    tests = [{
+                'input':
+                {
+                    'nums':[0,1,3,5,6,9,10],
+                    'target': 9
+                },
+                'output': 5
+				
+            },
+            {
+                'input':
+                {
+                    'nums':[1,2,4,6,8,12,13],
+                    'target': 10
+                },
+                'output': -1
+				
+            }
+            ]
 
-		test = {
-						'input':
-						{
-							'nums' : [-1,0,3,5,9,12]
-							'target' : 9
-						}
-						'output': 4
-						}
-    def searchnum(self, nums: List[int], target: int) -> int:
+    def searchnum(nums, target) -> int:
         lo, hi = 0, len(nums) - 1
         
         #iterative approach
@@ -21,13 +33,13 @@ class BinarySearch:
                 lo = mid + 1
             elif target < nums[mid]:
                 hi = mid - 1
-        
+
         return -1
-      
-      def locate_num -> None:
+    
+    for test in tests:
         result = searchnum(**test['input'])
         if (result == test['output']):
-            print ('Success\n Input: {}\n Output: {}'.format(**test['input'],result))
-        elif (result == -1):
-            print ('Fail\n Input: {}\n Output: {}'.format(**test['input'],result))
-        
+            print ('Success\n Input: {input}\n Expected Output: {output}\n Actual Output: {}'.format(result,**test))
+        else:
+            print ('Fail')
+    

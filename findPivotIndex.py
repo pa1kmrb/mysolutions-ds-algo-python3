12daddbfef5fd2c5c6f4f375d814717eaa139287
Pavan Bodaballa <impkb19@gmail.com>
2022-06-21 01:08:03 -0400

    Modified to handle inputs with recurring elements

--- findPivotIndex.py
+++ findPivotIndex.py
@@ -11,10 +11,14 @@ class Solution:
         left, right = 0, sum(nums)
 
         #start from left, loop through the list incrementing left sum and decrementing right sum
-        for num in nums:
+        #enumerate list to have incremental index in case of duplicate values
+        for idx, num in enumerate(nums):
             right -= num
             if left == right:
-                return nums.index(num)
+                return idx
+                #below returns the first occurrence of element if the list has repeating elements - not correct
+                #[-1,-1,-1,-1,-1,0] expected value 2, but below statement returns 0 (first occurrance)
+                ##return nums.index(num)##
             left += num
         
         #return -1 if no index exists such that the sum of elements on either side of the index is equal
